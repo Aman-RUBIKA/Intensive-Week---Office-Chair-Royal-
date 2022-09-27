f1c7e2a117c5761bddb2331e9ba4d36ffe6e08a2
m.skutnik <m.skutnik@rubika-edu.com>
2022-09-27 19:13:32 +0200

    Melee ennemy can move
    no friction for when you push him however, he won't stop sliding and won't be able to come back

--- My project/Assets/Scripts/Enemy Script/meleeAI.cs
+++ My project/Assets/Scripts/Enemy Script/meleeAI.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class meleeAI : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float speed;
+    private Vector2 pPosition;
     private GameObject player;
     void Start()
     {
@@ -13,17 +16,18 @@ public class meleeAI : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        pPosition = player.transform.position;
         SeekPlayer();
     }
 
     void SeekPlayer()
     {
         float angle;
-        Vector2 playerPosition = player.transform.position;
-        angle =(Mathf.Atan2(transform.position.y - playerPosition.y, transform.position.x - playerPosition.x));
+        float step = speed * Time.deltaTime;
+        angle =(Mathf.Atan2(transform.position.y - pPosition.y, transform.position.x - pPosition.x));
         transform.rotation = Quaternion.Euler(0, 0, (Mathf.Rad2Deg * angle) - 90);
+        // turn towards player
+        transform.position = Vector2.MoveTowards(transform.position, pPosition, step);
     }
-    
-    
     
 }
