2efcdeed377e57f61fb463778d8fe37e11649174
Luc Carrant <l.carrant@rubika-edu.com>
2022-09-28 16:58:06 +0200

    st

---
+++ My project/Assets/Scripts/Shop/Upgradebuy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Upgradebuy : MonoBehaviour
+{
+    [SerializeField] Sprite  mage;
+    // Start is called before the first frame update
+    public void Set(UpgradeManager upgradeManager)
+    {
+        mage = UpgradeManager.icon;
+    }
+}
