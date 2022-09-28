ee30d6776a7b9c35e67b3cca3b14c3aa313afa6b
m.skutnik <m.skutnik@rubika-edu.com>
2022-09-28 18:29:28 +0200

    haha crash go brrrrrrr
    no crash no more tho :(

--- My project/Assets/Scripts/EnemyManager.cs
+++ My project/Assets/Scripts/EnemyManager.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-
+    [SerializeField] private GameObject player ;
+    private Vector2 pPosition;
     [SerializeField] private GameObject[] enemyPrefabs;
+    [SerializeField] private bool BossPresent;
     public int totalAppeared;
     public int meleePresent;
     private int meleeKilled;
@@ -13,21 +15,75 @@ public class EnemyManager : MonoBehaviour
     private int rangerKilled;
     public int explosivePresent;
     private int explosiveKilled;
-    public int survivedWaves;
+    public int survivedWaves = 0;
     
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
+        pPosition = player.transform.position;
+        SpawnNewEnemy();
     }
 
     void SpawnNewEnemy()
     {
-        if (rangerPresent < survivedWaves + 1)
+        if (BossPresent)
         {
-            Instantiate(enemyPrefabs[2]);
+            
+        }
+        else
+        {
+            while (meleePresent + rangerPresent + explosivePresent < survivedWaves + 4)
+            {
+                if (explosiveKilled >= 3)
+                {
+                    oocInstance(enemyPrefabs[3]);
+                    explosiveKilled -= 3;
+                    rangerPresent += 1;
+                }
+
+                else if (meleeKilled >= 5)
+                {
+                    oocInstance(enemyPrefabs[2]);
+                    meleeKilled -= 5;
+                    explosivePresent += 1;
+                }
+                else
+                {
+                    oocInstance(enemyPrefabs[0]);
+                    meleePresent += 1;
+                }
+            } 
         }
     }
-    
+
+    void oocInstance(GameObject go)
+    {
+        GameObject cam = GameObject.FindWithTag("MainCamera");
+        float height = cam.GetComponent<Camera>().orthographicSize * 2f ;
+        float Width = height * cam.GetComponent<Camera>().aspect ;
+        float widthFromPlayer = Width / 2;
+        float heightFromPlayer = height / 2;
+        float randomvalue = Random.value;
+        
+
+        int whereSpawn = Random.Range(1, 5);// 1, up / 2, left / 3, down / 4, right
+        switch (whereSpawn)
+        {
+            case 1:
+                Instantiate(go, new Vector2(randomvalue * Width, pPosition.y + heightFromPlayer + 1), Quaternion.identity);
+                break;
+            case  2:
+                Instantiate(go, new Vector2(pPosition.x + widthFromPlayer + 1, randomvalue * height), Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(go, new Vector2(randomvalue * Width, pPosition.y - heightFromPlayer - 1), Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(go, new Vector2(pPosition.x - widthFromPlayer - 1, randomvalue * height), Quaternion.identity);
+                break;
+        }
+        
+    }
     
     
     void Update()
