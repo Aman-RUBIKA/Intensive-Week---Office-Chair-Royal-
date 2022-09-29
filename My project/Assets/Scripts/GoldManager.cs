c93d17c2879155a594aeb917b4585deee6249c9d
m.skutnik <m.skutnik@rubika-edu.com>
2022-09-29 17:11:39 +0200

    Gold manager créé, fonctionnel
    faut mettre dans une scène et lui donner un texte

---
+++ My project/Assets/Scripts/GoldManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldManager : MonoBehaviour
+{
+    private float currentGold;
+    public Text textGold;
+
+    public void AddGold(int acquired)
+    {
+        currentGold += acquired;
+        textGold.text = "$ :" + currentGold.ToString();
+        Debug.Log(currentGold);
+
+    }
+    
+    
+    
+    
+}
